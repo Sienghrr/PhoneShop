cd20f747c5f1438dae72394eeb78e35abb3dc5f2
Sienghrr <154961774+Sienghrr@users.noreply.github.com>
2024-12-19 21:56:13 +0700

    Update SecurityConfig.java

--- src/main/java/com/sieng/java/phoneshop_sieng/config/security/SecurityConfig.java
+++ src/main/java/com/sieng/java/phoneshop_sieng/config/security/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableGlobalMethodSecurity(
 		  prePostEnabled = true, 
 		  securedEnabled = true, 
-		  jsr250Enabled = true)
+		  jsr250Enabled = true) // use when we want to antMatcher on method(Post,Get..) and use with PreAuthories
 public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
@@ -29,7 +29,7 @@ protected void configure(HttpSecurity http) throws Exception {
 		.antMatchers("/","index.html","css/**","js/**").permitAll()
 		//.antMatchers("/brands").hasRole("SALE")
 		//.antMatchers(HttpMethod.POST,"/brands").hasAuthority("brand:write")
-		//.antMatchers("/models").hasRole(RoleEnum.SALE.name()) // SALE
+		//.antMatchers("/models").hasRole(RoleEnum.SALE.name()) // SALE // can use @ruleallow on method or class instead 
 		//.antMatchers(HttpMethod.POST,"/brands").hasAuthority(BRAND_WRITE.getDescription())
 		//.antMatchers(HttpMethod.GET,"/brands").hasAuthority(BRAND_READ.getDescription())
 		.anyRequest()
